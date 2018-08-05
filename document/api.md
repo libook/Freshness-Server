a1c0f24feb24fc928924084e4c002732954c2c46
libook <libook7@gmail.com>
2018-08-05 21:22:01 +0800

    Basic WEB server.

---
+++ document/api.md
@@ -0,0 +1,14 @@
+
+---
+## GET /
+
+API for checking whether server is running.
+
+### Response:
+
+#### Body:
+
+```javascript
+ "Welcome to Freshness Server."
+```
+
--- source/server.js
+++ source/server.js
@@ -2,4 +2,29 @@
  * This is the entrance fo the server.
  */
 
+const Koa = require('koa');
+const KoaRouter = require('koa-router');
 
+const PORT = 3000;
+
+const app = new Koa();
+const router = new KoaRouter();
+
+/**
+ * @uri
+ * API for checking whether server is running.
+ * @get /
+ * @response
+ *      @body
+ *          "Welcome to Freshness Server."
+ */
+router.get('/', (ctx) => {
+    ctx.body = "Welcome to Freshness Server.";
+});
+
+app
+    .use(router.routes())
+    .use(router.allowedMethods());
+
+app.listen(PORT);
+console.log(PORT);
